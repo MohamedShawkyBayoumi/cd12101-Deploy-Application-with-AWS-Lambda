75b6cbe18846026128c6c0104ba26dbf40d28f27
Mohamed Hassan <mohamedhassan@xnmh7nhyyg.localdomain>
2024-07-21 22:01:41 +0200

    Add apiId and Auth0 parameters for the reviewer

---
+++ starter/client/config.ts
@@ -0,0 +1,7 @@
+export const apiEndpoint = `https://an9psm8pri.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-aciclnd7pdtnssfk.us.auth0.com', // Auth0 domain
+  clientId: '3wNc3OB1X6GVdGsa3ZLsIt2JWDlts6x9', // Auth0 client ID
+  callbackUrl: 'http://localhost:3000'
+}
